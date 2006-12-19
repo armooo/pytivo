cd5239d94142323404481601f56fb999acc3a768
Jason Michalski <armooo@armooo.net>
2006-12-19 04:46:30 +0000

    pyTivo - Missed a file

---
+++ plugin.py
@@ -0,0 +1,13 @@
+def GetPlugin(name):
+    module_name = '.'.join(['plugins', name, name])
+    module = __import__(module_name, fromlist=name)
+    plugin = getattr(module, name)()
+    return plugin
+
+class Plugin:
+
+    commands = {}
+    content_type = ''
+
+    def SendFile():
+        pass
