51b0b5e0861235af31b4eb5f82a1543bc61be6ba
KRKeegan <kevin@krkeegan.com>
2008-02-11 20:56:50 -0800

    New: PreCache Files so TiVo Loads Faster
    This will cause pyTivo parse all files recursivly in a share to load them into the cache.  Currently this only works in the video plugin, but incorporation into other plugins that use a cache is very easy.
    
    In the video plugin this causes pyTivo to run ffmpeg on EVERY video file in that share and stores that information in the cache.
    
    Benefits: This greatly decreases the load time while browsing on the TiVo. For me a folder with 50 videos would take 18 seconds to load the first time I viewed it.  Now it takes less than 2 seconds.
    Drawbacks: This will create a delay in the startup of pyTivo while processing the files.  For me I experienced a delay of nearly 2 minutes to parse 757 video files.
    
    Usage:
    Set the precache=true under any share.  If the plugin does not support the precache setting it is ignored.
    
    Example:
    In the following conf file the share "Old TV" would be precached while "Movies" and "Photos" would not be.
    
    [Old TV]
    type = video
    path = /store/TV
    precache=true
    
    [Movies]
    type = video
    path = /store/Movies
    
    [Photos]
    type = photo
    path = /store/Photos
    
    [Server]
    debug = true
    ffmpeg = /usr/bin/ffmpeg
    hack83 = true
    port = 9032

--- pyTivo.py
+++ pyTivo.py
@@ -2,6 +2,7 @@
 
 import beacon, httpserver, os, sys
 import config
+from plugin import GetPlugin
 
 port = config.getPort()
 
@@ -9,6 +10,23 @@ httpd = httpserver.TivoHTTPServer(('', int(port)), httpserver.TivoHTTPHandler)
 
 for section, settings in config.getShares():
     httpd.add_container(section, settings)
+    #Precaching of files: does a recursive list of base path
+    if settings.get('precache', 'False').lower() == 'true':
+        plugin = GetPlugin(settings.get('type'))
+        if hasattr(plugin,'pre_cache'):
+            pre_cache_filter = getattr(plugin, 'pre_cache')
+            
+            def build_recursive_list(path):
+                files = []
+                for file in os.listdir(path):
+                    file = os.path.join(path, file)
+                    if os.path.isdir(file):
+                        files.extend(build_recursive_list(file))
+                        continue
+                    pre_cache_filter(file)
+                return files
+
+            files = build_recursive_list(settings.get('path'))
 
 b = beacon.Beacon()
 b.add_service('TiVoMediaServer:' + str(port) + '/http')
