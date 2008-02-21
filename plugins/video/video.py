7698eb69bcf50924e32f693c40db256c32c85671
William McBrine <wmcbrine@gmail.com>
2008-02-21 00:47:58 -0500

    Neater.

--- plugins/video/video.py
+++ plugins/video/video.py
@@ -65,7 +65,8 @@ class Video(Plugin):
 
         # not a tivo
         if not tsn:
-            debug_write(['Hack this was not a TiVo request. Using default tsn.', '\n'])
+            debug_write(['Hack this was not a TiVo request.',
+                         'Using default tsn.\n'])
             tsn = '123456789'
 
         # this breaks up the anchor item request into seperate parts
